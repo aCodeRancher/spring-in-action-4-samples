97b6a3041cfc4acab2326b2efbe3ad624360943b
helen <hhma@alumni.cpp.edu>
2019-05-04 19:22:03 -0400

    use FileSystemXmlApplicationContext

--- Chapter_01/knight/src/main/java/sia/knights/KnightMain.java
+++ Chapter_01/knight/src/main/java/sia/knights/KnightMain.java
@@ -3,13 +3,13 @@ package sia.knights;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.
                    ClassPathXmlApplicationContext;
-
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class KnightMain {
 
   public static void main(String[] args) throws Exception {
     ApplicationContext context =
-        new ClassPathXmlApplicationContext(
-            "META-INF/spring/knight.xml");
+        new FileSystemXmlApplicationContext(
+            "classpath:META-INF/spring/knight.xml");
     Knight knight = (Knight)context.getBean("knight1");
     knight.embarkOnQuest();
 
